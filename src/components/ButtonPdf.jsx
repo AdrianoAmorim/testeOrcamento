ac8f631f722d99505f0ccfc446c23bc1158c674a
AdrianoAmorim <adryanoamorim@hotmail.com>
2023-08-29 22:28:33 -0300

    ajustes botao return

--- src/components/ButtonPdf.jsx
+++ src/components/ButtonPdf.jsx
@@ -7,7 +7,7 @@ const ButtonPdf = ()=>{
         <PDFDownloadLink document={<DocumentPDF />} fileName="Orçamento-Facil.pdf">
         {({ loading,  }) =>
           loading ? 
-          <button className='p-3 text-[18px] font-semibold rounded bg-green-600 text-white shadow-md' >Aguarde...</button> : 
+          <button className='p-3 text-[18px] font-semibold rounded bg-green-600 text-white shadow-md' >Aguarde..</button> : 
           <button className='p-3 text-[18px] font-semibold rounded bg-green-600 text-white shadow-md' >
             <IconDownloadPdf width="32px" height="32px"/>
           </button>
